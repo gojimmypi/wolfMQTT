729fade87e690ec528d1b249f9cad05e567e0509
gojimmypi <jim@wolfssl.com>
2024-01-08 12:18:54 -0800

    build template create-project-from-example

--- IDE/Espressif/ESP-IDF/compileAllExamples.sh
+++ IDE/Espressif/ESP-IDF/compileAllExamples.sh
@@ -3,7 +3,9 @@
 # wolfMQTT testing script: compileAllExamples
 #
 
-echo "Starting wolfMQTT compileAllExamples.sh"
+echo "************************************************************************"
+echo "* Starting wolfMQTT compileAllExamples.sh"
+echo "************************************************************************"
 
 if  [[ "$IDF_PATH" == "" ]]; then
     echo "Error: $IDF_PATH not found; run Espressif export.sh"
@@ -15,3 +17,14 @@ RUN_SETUP=$1
 THIS_ERR=0
 
 echo "Found IDF_PATH = $IDF_PATH"
+
+echo "************************************************************************"
+echo "* template create-project-from-example"
+echo "************************************************************************"
+
+idf.py create-project-from-example "wolfssl/wolfssl^5.6.6-stable-update2-esp32:template"
+
+cd template
+
+idf.py build
+
